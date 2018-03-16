1289a6177f4f0be5290efbf94d5e47b69b2d66f9
max-mironov <v-maximi@microsoft.com>
2018-03-16 11:34:18 +0300

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,16 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('Congratulations, your extension "vscode-appcenter" is now active!');
+
+    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+        vscode.window.showInformationMessage('Hello World!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
