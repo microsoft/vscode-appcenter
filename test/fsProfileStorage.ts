df64eec07073392511ded9689e81f4a0d55931a7
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-04-11 15:07:25 +0300

    Added tests for several more methods.

--- test/fsProfileStorage.ts
+++ test/fsProfileStorage.ts
@@ -1,6 +1,6 @@
-
 import should = require('should');
 import sinon = require('sinon');
+
 import FsProfileStorage from '../src/helpers/fsProfileStorage';
 import { FSUtils } from '../src/helpers/fsUtils';
 import { VstsProfile } from '../src/helpers/interfaces';
@@ -106,6 +106,124 @@ describe('FileStore', function () {
       const activeProfile = vstsProfileStorage.activeProfile;
       should.deepEqual(activeProfile, currentActiveProfile);
       saveProfilesStub.calledTwice.should.be.true();
+    });
+  });
+
+  describe('#delete', () => {
+    let vstsProfileStorage: FsProfileStorage<VstsProfile>;
+    let profiles: VstsProfile[];
+    let saveProfilesStub: sinon.stub;
+
+    before(() => {
+      profiles = require(mockFilePath);
+    });
+
+    beforeEach(async () => {
+      const absolutePath = path.resolve("test/" + mockFilePath);
+      vstsProfileStorage = new FsProfileStorage(absolutePath, new ConsoleLogger());
+      saveProfilesStub = sandbox.stub(vstsProfileStorage, 'saveProfiles');
+      saveProfilesStub.resolves();
+      await vstsProfileStorage.init();
+    });
+
+    after(() => {
+      sandbox.restore();
+    });
+
+    it('should delete active profile', async () => {
+      const activeProfile = profiles.filter(profile => profile.isActive)[0];
+      const deletedProfile = await vstsProfileStorage.delete(activeProfile.userId);
+      const currentActiveProfile = vstsProfileStorage.activeProfile;
+      should.deepEqual(currentActiveProfile, null);
+      should.deepEqual(deletedProfile, activeProfile);
+      saveProfilesStub.calledOnce.should.be.true();
+    });
+
+    it('should delete non-active profile and preserve the active one', async () => {
+      const nonActiveProfile = profiles.filter(profile => !profile.isActive)[0];
+      const activeProfile = profiles.filter(profile => profile.isActive)[0];
+      const deletedProfile = await vstsProfileStorage.delete(nonActiveProfile.userId);
+      const currentActiveProfile = vstsProfileStorage.activeProfile;
+      should.deepEqual(activeProfile, currentActiveProfile);
+      should.deepEqual(deletedProfile, nonActiveProfile);
+      saveProfilesStub.calledOnce.should.be.true();
+    });
+
+    it('should not delete the profile thats not exist', async () => {
+      const deletedProfile = await vstsProfileStorage.delete("fakeUserId");
+      should.equal(deletedProfile, null);
+      saveProfilesStub.called.should.be.false();
+    });
+  });
+
+  describe('#get', () => {
+    let vstsProfileStorage: FsProfileStorage<VstsProfile>;
+    let profiles: VstsProfile[];
+    let malformedProfiles: VstsProfile[];
+
+    before(() => {
+      profiles = require(mockFilePath);
+      malformedProfiles = require(mockFilePathMalformedDuplicateUserIds);
+    });
+
+    beforeEach(async () => {
+      const absolutePath = path.resolve("test/" + mockFilePath);
+      vstsProfileStorage = new FsProfileStorage(absolutePath, new ConsoleLogger());
+      await vstsProfileStorage.init();
+    });
+
+    after(() => {
+      sandbox.restore();
+    });
+
+    it('should return null if profile not found', async () => {
+      const foundProfile = await vstsProfileStorage.get("fakeUserId");
+      should.deepEqual(foundProfile, null);
+    });
+
+    it('should throw if more than one profile found', async () => {
+      const absolutePath = path.resolve("test/" + mockFilePathMalformedDuplicateUserIds);
+      vstsProfileStorage = new FsProfileStorage(absolutePath, new ConsoleLogger());
+      await vstsProfileStorage.init();
+      const duplicateProfile: VstsProfile = malformedProfiles[0];
+      return new Promise(function (resolve, reject) {
+        vstsProfileStorage.get(duplicateProfile.userId).catch(error => {
+          should.exist(error);
+          resolve();
+        }).then(() => {
+          reject("the function should throw");
+        });
+      });
+    });
+
+    it('should return valid profile', async () => {
+      const profile = profiles[0];
+      const foundProfile = await vstsProfileStorage.get(profile.userId);
+      should.deepEqual(foundProfile, profile);
+    });
+  });
+
+  describe('#list', () => {
+    let vstsProfileStorage: FsProfileStorage<VstsProfile>;
+    let profiles: VstsProfile[];
+
+    before(() => {
+      profiles = require(mockFilePath);
+    });
+
+    beforeEach(async () => {
+      const absolutePath = path.resolve("test/" + mockFilePath);
+      vstsProfileStorage = new FsProfileStorage(absolutePath, new ConsoleLogger());
+      await vstsProfileStorage.init();
+    });
+
+    after(() => {
+      sandbox.restore();
+    });
+
+    it('should list profiles', async () => {
+      const listedProfiles: VstsProfile[] = await vstsProfileStorage.list();
+      should.deepEqual(listedProfiles, profiles);
     });
   });
 });
