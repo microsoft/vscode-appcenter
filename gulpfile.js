55e0716e53f841aaaa34eaa8a627e5ed24638189
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-04-25 12:51:15 +0300

    Deleted nexpect

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,6 @@ gulp.task("tslint", function () {
 gulp.task("clean", function () {
     var pathsToDelete = [
         "src/**/*.js",
-        "!src/helpers/nexpect.js",
         "!src/appcenter/apis/generated/**/*.js",
         "src/**/*.js.map",
         "test/**/*.js",
--- src/appcenter/commands/tools/linkCodePush.ts
+++ src/appcenter/commands/tools/linkCodePush.ts
@@ -59,29 +59,34 @@ export default class LinkCodePush extends ReactNativeAppCommand {
 
     protected async handleShowCurrentAppQuickPickSelection(selected: QuickPickAppItem, _rnApps: models.AppResponse[]) {
         this.userAlreadySelectedApp = false;
-        const selectedApps: models.AppResponse[] = _rnApps.filter(app => app.name === selected.target && app.owner.type === selected.description);
-        if (!selectedApps || selectedApps.length !== 1) {
-            this.showedCount = 0;
-            this.pickedApps = [];
-            return;
+
+        let currentApp: CurrentApp | null;
+        if (selected.target === this.currentAppMenuTarget) {
+            currentApp = await this.getCurrentApp();
+        } else {
+            const selectedApps: models.AppResponse[] = _rnApps.filter(app => app.name === selected.target && app.owner.type === selected.description);
+            if (!selectedApps || selectedApps.length !== 1) {
+                this.showedCount = 0;
+                this.pickedApps = [];
+                return;
+            }
+            const selectedApp: models.AppResponse = selectedApps[0];
+            const selectedAppName: string = `${selectedApp.owner.name}/${selectedApp.name}`;
+            const selectedAppSecret: string = selectedApp.appSecret;
+            const type: string = selectedApp.owner.type;
+
+            const OS: AppCenterOS | undefined = Utils.toAppCenterOS(selectedApp.os);
+            currentApp = Utils.toCurrentApp(selectedAppName, OS, null, Constants.AppCenterDefaultTargetBinaryVersion, type, Constants.AppCenterDefaultIsMandatoryParam, selectedAppSecret);
         }
-        const selectedApp: models.AppResponse = selectedApps[0];
-        const selectedAppName: string = `${selectedApp.owner.name}/${selectedApp.name}`;
-        const selectedAppSecret: string = selectedApp.appSecret;
-        const type: string = selectedApp.owner.type;
-
-        const OS: AppCenterOS | undefined = Utils.toAppCenterOS(selectedApp.os);
-        const currentApp = Utils.toCurrentApp(selectedAppName, OS, null, Constants.AppCenterDefaultTargetBinaryVersion, type, Constants.AppCenterDefaultIsMandatoryParam, selectedAppSecret);
-
         this.pickedApps.push(currentApp);
         if (this.showedCount < 1) {
             this.showedCount++;
-            const missingOS: AppCenterOS = OS === AppCenterOS.Android ? AppCenterOS.iOS : AppCenterOS.Android;
+            const missingOS: AppCenterOS = currentApp.os.toLowerCase() === AppCenterOS.Android.toLowerCase() ? AppCenterOS.iOS : AppCenterOS.Android;
             const cachedFilteredApps = this.CachedApps.filter((cachedApp) => {
                 return cachedApp.os.toLowerCase() === missingOS.toLowerCase();
             });
-            const currentApp: CurrentApp | null = await this.getCurrentApp();
-            const showCurrentApp: boolean = currentApp.os.toLowerCase() !== OS.toLowerCase();
+            const current: CurrentApp | null = await this.getCurrentApp();
+            const showCurrentApp: boolean = current.os.toLowerCase() !== currentApp.os.toLowerCase();
             this.showAppsQuickPick(cachedFilteredApps, showCurrentApp, false, Strings.ProvideSecondAppPromptMsg);
         } else {
             this.linkApps();
