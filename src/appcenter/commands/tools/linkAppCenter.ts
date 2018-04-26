b81cb75e6f34c762dddffa1d82a1830a12861c68
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-04-26 15:43:54 +0300

    To last

--- src/appcenter/commands/tools/linkAppCenter.ts
+++ src/appcenter/commands/tools/linkAppCenter.ts
@@ -38,40 +38,4 @@ export default class LinkAppCenter extends LinkCommand {
 
         return await appCenterLinker.linkAppCenter(this.pickedApps);
     }
-
-    protected async handleShowCurrentAppQuickPickSelection(selected: QuickPickAppItem, _rnApps: models.AppResponse[]) {
-        this.userAlreadySelectedApp = false;
-
-        let currentApp: CurrentApp | null;
-        if (selected.target === this.currentAppMenuTarget) {
-            currentApp = await this.getCurrentApp();
-        } else {
-            const selectedApps: models.AppResponse[] = _rnApps.filter(app => app.name === selected.target && app.owner.type === selected.description);
-            if (!selectedApps || selectedApps.length !== 1) {
-                this.showedCount = 0;
-                this.pickedApps = [];
-                return;
-            }
-            const selectedApp: models.AppResponse = selectedApps[0];
-            const selectedAppName: string = `${selectedApp.owner.name}/${selectedApp.name}`;
-            const selectedAppSecret: string = selectedApp.appSecret;
-            const type: string = selectedApp.owner.type;
-
-            const OS: AppCenterOS | undefined = Utils.toAppCenterOS(selectedApp.os);
-            currentApp = Utils.toCurrentApp(selectedAppName, OS, null, Constants.AppCenterDefaultTargetBinaryVersion, type, Constants.AppCenterDefaultIsMandatoryParam, selectedAppSecret);
-        }
-        this.pickedApps.push(currentApp);
-        if (this.showedCount < 1) {
-            this.showedCount++;
-            const missingOS: AppCenterOS = currentApp.os.toLowerCase() === AppCenterOS.Android.toLowerCase() ? AppCenterOS.iOS : AppCenterOS.Android;
-            const cachedFilteredApps = this.CachedApps.filter((cachedApp) => {
-                return cachedApp.os.toLowerCase() === missingOS.toLowerCase();
-            });
-            const current: CurrentApp | null = await this.getCurrentApp();
-            const showCurrentApp: boolean = current.os.toLowerCase() !== currentApp.os.toLowerCase();
-            this.showAppsQuickPick(cachedFilteredApps, showCurrentApp, false, Strings.ProvideSecondAppPromptMsg);
-        } else {
-            this.linkApps();
-        }
-    }
 }
