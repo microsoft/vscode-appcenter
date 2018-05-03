b5c485b03463b0460dc980177c7c5bda05b203c6
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-05-03 11:35:29 +0300

    Got rid of MenuHelper (#57)

--- src/menu/menu.ts
+++ src/menu/menu.ts
@@ -1,8 +1,10 @@
 import * as vscode from "vscode";
+import { models } from "../appcenter/apis";
 import { AppCenterBeacons, AppCenterCrashesTabs, AppCenterDistributionTabs, CommandNames } from "../constants";
 import { FSUtils } from "../helpers/fsUtils";
-import { CommandParams, CurrentApp, MenuQuickPickItem } from "../helpers/interfaces";
+import { CommandParams, CurrentApp, MenuQuickPickItem, Profile, UserOrOrganizationItem  } from "../helpers/interfaces";
 import { Utils } from "../helpers/utils";
+import { CustomQuickPickItem } from "../helpers/vsCodeUtils";
 import { ILogger } from "../log/logHelper";
 import { Strings } from "../strings";
 
@@ -234,4 +236,68 @@ export class MenuItems {
         description: Strings.HideStatusBarMenuDescription,
         command: CommandNames.Settings.HideStatusBar
     };
+}
+
+export function getSelectedUserOrOrgItem(selected: CustomQuickPickItem, allItems: CustomQuickPickItem[]): UserOrOrganizationItem | null {
+    let userOrOrgItem: UserOrOrganizationItem;
+    const selectedUserOrOrgs: CustomQuickPickItem[] = allItems.filter(item => item.target === selected.target);
+    if (selectedUserOrOrgs && selectedUserOrOrgs.length === 1) {
+        userOrOrgItem = {
+            name: selectedUserOrOrgs[0].target,
+            displayName: selectedUserOrOrgs[0].label,
+            isOrganization: selectedUserOrOrgs[0].description !== Strings.UserMenuDescriptionLabel
+        };
+        return userOrOrgItem;
+    } else {
+        return null;
+    }
+}
+
+export function getCreateAppOptions(): vscode.QuickPickItem[] {
+    const createAppOptions: vscode.QuickPickItem[] = [];
+    createAppOptions.push(<vscode.QuickPickItem>{
+        label: Strings.CreateNewAndroidAppMenuLabel,
+        description: "",
+        target: CommandNames.CreateApp.Android
+    });
+    createAppOptions.push(<vscode.QuickPickItem>{
+        label: Strings.CreateNewIOSAppMenuLabel,
+        description: "",
+        target: CommandNames.CreateApp.IOS
+    });
+    createAppOptions.push(<vscode.QuickPickItem>{
+        label: Strings.CreateNewAppsForBothMenuLabel,
+        description: "",
+        target: CommandNames.CreateApp.Both
+    });
+    return createAppOptions;
+}
+
+export function getQuickPickItemsForAppsList(appsList: models.AppResponse[]) {
+    return appsList.map((app: models.AppResponse) => {
+        return {
+            label: `${app.name} (${app.os})`,
+            description: app.owner.type,
+            target: `${app.name}`
+        };
+    });
+}
+
+export function getQuickPickItemsForOrgList(orgList: models.ListOKResponseItem[], myself: Profile | null): CustomQuickPickItem[] {
+    const options: CustomQuickPickItem[] = orgList.map(item => {
+        return {
+            label: `${item.displayName} (${item.name})`,
+            description: Strings.OrganizationMenuDescriptionLabel,
+            target: item.name
+        };
+    });
+    if (myself) {
+        // Insert user at the very 1st position
+        options.splice(0, 0, {
+            label: `${myself.displayName}`,
+            description: Strings.UserMenuDescriptionLabel,
+            target: myself.userName
+        });
+    }
+    return options;
 }
