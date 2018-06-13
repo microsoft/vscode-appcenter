b7c5c6e1df24fc12da69b45457a101f5440fa38e
Evgeniy Poletaikin <jekapolet777@gmail.com>
2018-06-13 17:37:33 +0300

    create 'telemetry source name' constant

--- src/api/appcenter/telemetry-filter.ts
+++ src/api/appcenter/telemetry-filter.ts
@@ -4,6 +4,7 @@
 
 import { WebResource } from "ms-rest";
 import { Readable, Writable } from "stream";
+import { Constants } from "../../extension/resources/constants";
 
 // tslint:disable:no-var-requires
 const requestPipeline = require("ms-rest/lib/requestPipeline");
@@ -15,7 +16,7 @@ export function telemetryFilter(): { (resource: WebResource, next: any, callback
     return (resource: WebResource, next: any, callback: any): any => {
         return requestPipeline.interimStream((input: Readable, output: Writable) => {
             input.pause();
-            resource.headers["internal-request-source"] = "vscode";
+            resource.headers["internal-request-source"] = Constants.TelemetrySource;
             resource.headers["diagnostic-context"] = sessionId;
             const nextStream = next(resource, callback);
             (resource.pipeInput(input, nextStream) as any as Readable).pipe(output);
