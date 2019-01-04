373208590d9d5fd7baf431f01b486c92dddb17ef
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-01-04 14:49:48 +0300

    Fix 20

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 CODE_TESTS_PATH=./integrationTest
 CODE_TESTS_WORKSPACE=./integrationTest/reactNativeApp
 npm i npm@latest -g
-
-
-
-npm config delete prefix 
-sudo npm install
-sudo npm install -g gulp-cli
-sudo npm install -g vsce
+npm install
+npm install -g gulp-cli
+npm install -g vsce
 npm run vscode:prepublish
 npm run integrationTest
