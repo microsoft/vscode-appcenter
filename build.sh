7ebcbc12175d993d44b9e264c762ba57eb274b38
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-01-04 13:58:18 +0300

    Build

---
+++ build.sh
@@ -0,0 +1,7 @@
+CODE_TESTS_PATH=./integrationTest
+CODE_TESTS_WORKSPACE=./integrationTest/reactNativeApp
+npm install
+npm install -g gulp-cli
+npm install -g vsce
+npm run vscode:prepublish
+npm run integrationTest
