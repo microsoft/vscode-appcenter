5846c6fe9aa3ada40ba563940269cad1fee34663
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-01-04 14:18:47 +0300

    Fix 3

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 CODE_TESTS_PATH=./integrationTest
 CODE_TESTS_WORKSPACE=./integrationTest/reactNativeApp
 
-cd /usr/local
+cd /usr/local/Cellar/node@8/
 ls
 $ npm config delete prefix 
-$ npm config set prefix /usr/local/versions/node/v6.11.1
+$ npm config set prefix /usr/local/Cellar/node@8/8.11.3
 npm install
 npm install -g gulp-cli
 npm install -g vsce
