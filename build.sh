66aeb019037b05d523e489e57e0df89fea7a03e4
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-01-04 17:12:13 +0300

    Test

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 CODE_TESTS_PATH=./integrationTest
 CODE_TESTS_WORKSPACE=./integrationTest/reactNativeApp
+echo $CODE_TESTS_WORKSPACE
 npm i npm@latest -g
 npm install
 npm install -g gulp-cli
 npm install -g vsce
 npm run vscode:prepublish
 rm -rf $NVM_DIR
-cd src
 ls
 sudo npm run integrationTest --loglevel verbose
