5ce2ed7eeec7ed6889ab7606793f9cd6b5af4c02
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-01-04 14:31:05 +0300

    Fix 9

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ CODE_TESTS_WORKSPACE=./integrationTest/reactNativeApp
  npm install -g n
 
 sudo n 8.11.3
-
-npm config delete prefix 
-npm config set prefix /usr/local/Cellar/node@8/8.11.3
 npm install
 npm install -g gulp-cli
 npm install -g vsce
