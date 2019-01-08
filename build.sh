9a36b22dcef8b4e7df2688da94fb564f8314e76b
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-01-08 11:43:41 +0300

    111

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-CODE_TESTS_PATH=$SYSTEM_BUILDPATH/integrationTest
-CODE_TESTS_WORKSPACE=$SYSTEM_BUILDPATH/integrationTest/reactNativeApp
+#CODE_TESTS_PATH=$SYSTEM_BUILDPATH/integrationTest
+#CODE_TESTS_WORKSPACE=$SYSTEM_BUILDPATH/integrationTest/reactNativeApp
+echo $CODE_TESTS_PATH
 npm i npm@latest -g
 npm install
 npm install -g gulp-cli
