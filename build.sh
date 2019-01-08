76e55e9b52b7e5105f9677c52c5a3034bb7fea64
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-01-08 11:51:48 +0300

    Final

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ npm install
 npm install -g gulp-cli
 npm install -g vsce
 npm run vscode:prepublish
-rm -rf $NVM_DIR
 node node_modules/vscode/bin/test
-#sudo npm run integrationTest --loglevel verbose --extensionTestsPath $CODE_TESTS_PATH
